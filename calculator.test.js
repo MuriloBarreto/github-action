62b2ef40e67aea916685082447e7608f24f8f394
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-09-15 21:56:05 -0300

    Realizando test

---
+++ calculator.test.js
@@ -0,0 +1,15 @@
+// import { test } from "@jest/globals"
+
+const calculator = require('./calculator');
+
+test("testando soma de 1+1", () =>{
+    expect(calculator.soma(1,1)).toBe(2);
+})
+
+test("testando divisao de 42/6", () =>{
+    expect(calculator.divisao(42,6)).toBe(7);
+})
+
+test("testando soma de 5+5", () =>{
+    expect(calculator.soma(5,5)).toBe(10);
+})
